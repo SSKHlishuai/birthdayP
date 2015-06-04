9e53fc6e6c0bb46d794661adf146253798ca530d
LiShuai <15801434001@163.com>
2015-06-04 10:45:06 +0800

    new BirthdayP

---
+++ BirthdayP/BirthdayP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BirthdayP
+//
+//  Created by mc on 15/6/4.
+//  Copyright (c) 2015年 mc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BirthdayP/BirthdayP/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  BirthdayP
+//
+//  Created by mc on 15/6/4.
+//  Copyright (c) 2015年 mc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
